f00e422ab94b3f802d8a03f203279f3e94ed5c87
Ponyashka7316 <yuriygurin1@mail.ru>
2018-10-06 11:12:22 +0300

    loop changed

--- Matches/Program.cs
+++ Matches/Program.cs
@@ -10,6 +10,18 @@ namespace Matches
     {
         static void Main(string[] args)
         {
+            int n = Int32.Parse(Console.ReadLine());
+            int matches = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i == 1)
+                    matches += 4;
+                else if (i % 2 == 0 && i > 2)
+                    matches += 2;
+                else
+                    matches += 3;
+            }
+            Console.WriteLine(matches);
         }
     }
 }
